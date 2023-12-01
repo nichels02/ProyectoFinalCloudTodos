0d2af258e8227484cb0b755c04b9f21d2b02e805
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2023-12-01 09:48:13 -0500

    NoveIA

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using AI.SteeringBehaviours;
 public class EnemyController : MonoBehaviour
 {
     // Start is called before the first frame update
